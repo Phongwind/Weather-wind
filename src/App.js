271d9a95bb6c0b6ba462f09ef78a407b21f1d100
Phongwind <phongdesign0201@gmail.com>
2020-03-18 22:36:54 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import ChoiceButtons from "./components/ChoiceButtons";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      bgImg: null,
+      weather: null,
+      temperature: "",
+      isLoading: true,
+      locationName: ""
+    };
+  }
+
+  componentDidMount = () => {
+    navigator.geolocation.getCurrentPosition(position => {
+      const { latitude, longitude } = position.coords;
+      this.getWeather(latitude, longitude);
+    });
+  };
+
+  
+  async getWeather(latitude, longitude) {
+    const API_KEY = "b275f7119d133735fa15ea68778f530b";
+    const url = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`;
+    let response = await fetch(url);
+    let data = await response.json();
+    this.setState({
+      locationName: data.name,
+      // put in more here
+    });
+  };
+  
+  
+  render() {
+
+    return (
+      <div className="container-fluid text-white my-auto">
+        <div className="container mx-auto my-4 py-4">
+          <div className="row justify-content-center text-center">
+            <h1 className="col-12 display-4 my-2 py-3 text-success">
+              Awesome Weather App
+            </h1>
+            <h2 className="col-12">Location Name</h2>
+            <h3 className="col-12 text-danger">Temperature</h3>
+            <h3 className="col-12">Weather Description</h3>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
-
 export default App;
